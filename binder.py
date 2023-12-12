a7966288fc6d95308a11b1f1dbfda4f722e7565d
M1yn4rz <bartosz.mlynarski.a@gmail.com>
2023-12-12 23:38:14 +0100

    Upgrade commit

--- binder.py
+++ binder.py
@@ -2,23 +2,16 @@ import pandas as pd
 
 
 
-def data_binder():
-
-    data = pd.read_csv('data.csv')
-
-    new_data = data.sort_values(by = ['Name', 'Surname'], ascending = True)
-
-    for i in range(len(new_data)):
-        new_data['ID'][i] = i + 1
-
-    new_data.to_csv('data.csv', index=False)
-
-
-
 def main():
 
-    data_binder()
+    data = pd.read_csv('data.csv')
+    new_data = data.copy()
+    new_data = new_data.sort_values(by=['Name', 'Surname'], ascending=True)
+    new_data['ID'] = range(1, len(new_data) + 1)
+    new_data.to_csv('data.csv', index = False)
 
 
 
-main()
+if __name__ == "__main__":
+
+    main()
